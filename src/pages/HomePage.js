7a58dd044d5496e33fe8a2654dc087dbd0b8f09b
Hollenstein Benjamin <benjamin.hollenstein@bbcag.ch>
2021-05-10 16:59:50 +0200

    GUI done without styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,6 +10,8 @@ function HomePage() {
                 <input type="text" value = {text} name="barcode" onChange={event => setText(event.target.value)} />
                 <button type="submit">Decodieren</button>
             </form>
+            <label>Decodierter Barcode: </label>
+            <textarea name = "decoded" rows = "10"></textarea>
         </div>
     )
 }
